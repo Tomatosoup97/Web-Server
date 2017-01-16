c864aade87cfdc9ad1e0d6448d1904d4f596df84
Mateusz Urbańczyk <murbanczyk@qed.ai>
2017-01-16 22:03:35 +0100

    Add status strings

--- src/status_code.hpp
+++ src/status_code.hpp
@@ -43,18 +43,14 @@ enum status_codes {
     HTTP_204_NO_CONTENT = 204,
     HTTP_205_RESET_CONTENT = 205,
     HTTP_206_PARTIAL_CONTENT = 206,
-    HTTP_207_MULTI_STATUS = 207,
     HTTP_300_MULTIPLE_CHOICES = 300,
     HTTP_301_MOVED_PERMANENTLY = 301,
     HTTP_302_FOUND = 302,
     HTTP_303_SEE_OTHER = 303,
     HTTP_304_NOT_MODIFIED = 304,
-    HTTP_305_USE_PROXY = 305,
-    HTTP_306_RESERVED = 306,
     HTTP_307_TEMPORARY_REDIRECT = 307,
     HTTP_400_BAD_REQUEST = 400,
     HTTP_401_UNAUTHORIZED = 401,
-    HTTP_402_PAYMENT_REQUIRED = 402,
     HTTP_403_FORBIDDEN = 403,
     HTTP_404_NOT_FOUND = 404,
     HTTP_405_METHOD_NOT_ALLOWED = 405,
@@ -70,22 +66,34 @@ enum status_codes {
     HTTP_415_UNSUPPORTED_MEDIA_TYPE = 415,
     HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE = 416,
     HTTP_417_EXPECTATION_FAILED = 417,
-    HTTP_422_UNPROCESSABLE_ENTITY = 422,
-    HTTP_423_LOCKED = 423,
-    HTTP_424_FAILED_DEPENDENCY = 424,
-    HTTP_428_PRECONDITION_REQUIRED = 428,
-    HTTP_429_TOO_MANY_REQUESTS = 429,
-    HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE = 431,
-    HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS = 451,
     HTTP_500_INTERNAL_SERVER_ERROR = 500,
     HTTP_501_NOT_IMPLEMENTED = 501,
     HTTP_502_BAD_GATEWAY = 502,
     HTTP_503_SERVICE_UNAVAILABLE = 503,
     HTTP_504_GATEWAY_TIMEOUT = 504,
     HTTP_505_HTTP_VERSION_NOT_SUPPORTED = 505,
-    HTTP_507_INSUFFICIENT_STORAGE = 507,
-    HTTP_511_NETWORK_AUTHENTICATION_REQUIRED = 511,  
 };
+
+// Status strings
+const std::string HTTP_200_OK = "200 OK";
+const std::string HTTP_201_CREATED = "201 Created";
+const std::string HTTP_202_ACCEPTED = "202 Accepted";
+const std::string HTTP_204_NO_CONTENT = "204 No Content";
+const std::string HTTP_300_MULTIPLE_CHOICES = "300 Multiple Choices";
+const std::string HTTP_301_MOVED_PERMANENTLY = "301 Moved Permanently";
+const std::string HTTP_302_FOUND = "302 Found";
+const std::string HTTP_304_NOT_MODIFIED = "304 Not Modified";
+const std::string HTTP_307_TEMPORARY_REDIRECT = "307 Temporary Redirect";
+const std::string HTTP_400_BAD_REQUEST = "400 Bad Request";
+const std::string HTTP_401_UNAUTHORIZED = "401 Unauthorized";
+const std::string HTTP_403_FORBIDDEN = "403 Forbidden";
+const std::string HTTP_404_NOT_FOUND = "404 Not Found";
+const std::string HTTP_500_INTERNAL_SERVER_ERROR = "500 Internal Server Error";
+const std::string HTTP_501_NOT_IMPLEMENTED = "501 Not Implemented";
+const std::string HTTP_502_BAD_GATEWAY = "502 Bad Gateway";
+const std::string HTTP_503_SERVICE_UNAVAILABLE = "503 Service Unavailable";
+const std::string HTTP_504_GATEWAY_TIMEOUT = "504 Gateway Timeout";
+const std::string HTTP_505_HTTP_VERSION_NOT_SUPPORTED = "505 HTTP version not supported";
 
 } // namespace status
 } // namespace webserver
