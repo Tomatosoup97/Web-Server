a2da7adb82f17c790fd37d69b698ba5e6b046922
Mateusz Urbańczyk <murbanczyk@qed.ai>
2017-01-23 23:14:20 +0100

    Http connection handler

--- src/connection.hpp
+++ src/connection.hpp
@@ -10,29 +10,29 @@
 
 namespace webserver {
 
+#define REQUEST_SIZE 16384 // 2^14
+
 typedef boost::shared_ptr<connection> connection_ptr;
+typedef boost::array<char, REQUEST_SIZE> request_buffer;
 
 
 class Connection : public boost::enable_shared_from_this<connection> {
     /* Class representing asynchronous TCP connection.
      *
      * Shared_ptr references are used in order to keep the connection
-     * alive. When no new async operation occurs, all shared_ptr disappear
+     * alive. When no new async operation occurs, all connection_ptr disappear
      * and destructor closes the socket.
      * */
 public:
-    Connection(boost::asio::io_service &io_service)
+    Connection(boost::asio::io_service &io_service,
+               RequestHandler request_handler)
             : strand_(io_service)
-            , socket_(io_service) {
+            , socket_(io_service)
+            , request_handler_(request_handler) {
     }
 
     void start() {
-        socket_.async_read_some(boost::asio::buffer(buffer_),
-            strand.wrap(
-                boost::bind(&connection::handle_read, shared_from_this(),
-                            boost::asio::placeholders::error,
-                            boost::asio::placeholders::bytes_transferred))
-            );
+        read_data();
     }
 
     boost::asio::ip::tcp::socket& socket() {
@@ -42,25 +42,58 @@ public:
 private:
     boost::asio::io_service::strand strand_;
     boost::asio::ip::tcp::socket socket_;
+    request_buffer buffer_;
     Request request_;
     Response response_;
+    RequestParser request_parser_;
+    RequestHandler request_handler_;
+
+    void read_data() {
+        socket_.async_read_some(boost::asio::buffer(buffer_),
+            strand.wrap(
+                boost::bind(&handle_reading_date, shared_from_this(),
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred))
+            );
+    }
+
+    void handle_reading_date(boost::system::error_code& e,
+                             std::size_t bytes_transferred) {
+        /* Handler which parse incoming request and return a response */
+        if (e) return;
+        parse_state = request_parser.parse(request_, buffer_.data(),
+                                           buffer_.data() + bytes_transferred);
+        switch (parse_state) {
+            case completed:
+                request_handler_.handle(request_, response_);
+                write_response();
+                break;
+            case unfinished:
+                read_data();
+                break;
+            case failed:
+                response_ = response.response(HTTP_400_BAD_REQUEST);
+                write_response();
+                break;
+            default:
+                respone_ = response.response(HTTP_500_INTERNAL_SERVER_ERROR);
+                write_response();
+        }
+    }
+
+    void write_response() {
+        boost::asio::async_write(socket_, response_.to_buffers(),
+            strand_.wrap(boost::bind(&shutdown_socket, shared_from_this(),
+                                     boost::asio::placeholders::error)));
+    }
 
     void shutdown_socket(boost::system::error_code& e) {
-        /* Disable both send and receive on the socket */
+        /* Disable both sending and receiving on the socket */
         if (e) return;
         boost::system::error_code error_code;
         socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
                          error_code);
-    }
-
-    void handle_read(boost::system::error_code& e) {
-        /* Parse incoming request and reply with a response */
-        if (e) return;
-        // TODO:
-        // Parse request
-        // Reply with a response
-        // Recursively invoke handle_read untill error occurs
-        // Continuously pass shared_ptr refrence
+        // connection_ptr reference disappear destroying the connection object
     }
 };
 
