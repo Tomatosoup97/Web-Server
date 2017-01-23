e9f5e3a74f2d30d66e855710c3415db05f3e41b5
Mateusz Urbańczyk <murbanczyk@qed.ai>
2017-01-24 00:41:28 +0100

    Interface for packing response into buffers
    + Change headers data structure

--- src/response.hpp
+++ src/response.hpp
@@ -12,10 +12,30 @@ public:
     int http_version;
     int http_subversion;
     std::string content;
-    std::map<std::string, std::string> headers;
+    std::vector<Header> headers;
     status::status_codes status_code;
 
-    Response() {
+    Response() {}
+
+    std::vector<boost::asio::const_buffer> as_buffers() {
+        /* Pack response into const buffers */
+        std::vector<boost::asio::const_buffer> buffers;
+        buffers.push_back(boost::asio::buffer(
+            status::verbose_status_code(status_code));
+        push_headers_as_buffers(buffers);
+        buffers.push_back(boost::asio::buffer(content));
+        return buffers
+    }
+
+private:
+    void push_headers_as_buffers(std::vector<boost::asio::const_buffer> buffers) {
+        for (std::size_t i=0; i < headers.size(); ++i) {
+            buffers.push_back(boost::asio::bufffer(headers[i].name));
+            buffers.push_back(boost::asio::bufffer(": "));
+            buffers.push_back(boost::asio::bufffer(headers[i].value));
+            buffers.push_back(boost::asio::bufffer("\r\n");
+        }
+        buffers.push_back(boost::asio::bufffer("\r\n");
     }
 };
 
